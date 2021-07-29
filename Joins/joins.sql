d7cb7e02af4c667badb08f81a83aa5aa6a3fa0e9
julienshim <25723428+julienshim@users.noreply.github.com>
2021-07-29 12:14:23 -0700

    as

--- Joins/joins.sql
+++ Joins/joins.sql
@@ -5,3 +5,7 @@ JOIN users on users.id = comments.user_id;
 SELECT contents, url
 FROM comments
 JOIN photos on photos.id = comments.photo_id;
+
+SELECT comments.id as comment_id, p.id
+FROM photos as pariatur --(FROM photos p)
+JOIN comments ON p.id = comments.photo_id;
