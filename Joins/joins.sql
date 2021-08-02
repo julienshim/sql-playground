c98f25d6228ff409b07b41520a066806cf76660e
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-02 10:00:04 -0700

    left right full joins

--- Joins/joins.sql
+++ Joins/joins.sql
@@ -9,3 +9,16 @@ JOIN photos on photos.id = comments.photo_id;
 SELECT comments.id as comment_id, p.id
 FROM photos as pariatur --(FROM photos p)
 JOIN comments ON p.id = comments.photo_id;
+
+-- INSERT INTO photos (url, user_id)
+-- VALUES ('https://banner.jpg', NULL);
+
+INSERT INTO users (username)
+VALUES ('Nicole');
+
+SELECT url, username
+FROM photos
+-- JOIN users on users.id = photos.user_id;
+-- LEFT JOIN users on users.id = photos.user_id;
+-- RIGHT JOIN users on users.id = photos.user_id;
+FULL JOIN users on users.id = photos.user_id;
