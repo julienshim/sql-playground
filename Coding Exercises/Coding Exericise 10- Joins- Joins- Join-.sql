31b36f05bbdce0d1666d95510c071f6d5b2f75a2
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-03 19:02:05 -0700

    rename

--- Coding Exercises/Coding Exericise 10- Joins- Joins- Join-.sql
+++
@@ -1,8 +0,0 @@
-SELECT title, name
-FROM authors
-LEFT JOIN books ON authors.id = books.author_id;
-
--- alt
--- SELECT title, name
--- FROM books
--- RIGHT JOIN authors ON books.author_id = authors.id;
