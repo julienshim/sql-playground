470671c9961f64f3c430a0f79a4a9d3d906f7d27
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-07 16:31:29 -0700

    spacing

--- Aggregations/group_by_with_having.sql
+++ Aggregations/group_by_with_having.sql
@@ -4,9 +4,8 @@ WHERE photo_id < 3
 GROUP BY photo_id
 HAVING COUNT(*) > 2;
 
-
 SELECT user_id, COUNT(*)
 FROM comments
-WHERE photo_id < 3
+WHERE photo_id <= 50
 GROUP BY user_id
-HAVING COUNT(*) > 2;
+HAVING COUNT(*) > 20;
