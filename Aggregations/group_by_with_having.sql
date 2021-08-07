1e4d2f61d4bd9b08c57fcf9e193a5ee6161dda47
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-07 16:30:55 -0700

    gbwh

--- Aggregations/group_by_with_having.sql
+++ Aggregations/group_by_with_having.sql
@@ -1,4 +1,12 @@
-SELECT photo_id, count(*)
-FROM comments WHERE photo_id < 3
+SELECT photo_id, COUNT(*)
+FROM comments 
+WHERE photo_id < 3
 GROUP BY photo_id
-HAVING photo_id > 2;
+HAVING COUNT(*) > 2;
+
+
+SELECT user_id, COUNT(*)
+FROM comments
+WHERE photo_id < 3
+GROUP BY user_id
+HAVING COUNT(*) > 2;
