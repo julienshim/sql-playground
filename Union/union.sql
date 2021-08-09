f4a5879a36fd086c3f4ff9e52760456c73aa4d3b
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-09 15:11:55 -0700

    exceptions

--- Union/union.sql
+++ Union/union.sql
@@ -1,11 +1,11 @@
 (
-  SELECT *
+  SELECT * -- matching column names and data types
   FROM products
   ORDER BY price DESC
   LIMIT 4
 )
 UNION -- ALL (do not remove duplicates)
-(
+( -- parentheses important to make keyword application clear
   SELECT *
   FROM products
   ORDER BY price / weight DESC
