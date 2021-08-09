b98a3c243f81b2ec79684abdcf9d94e7abcdc45f
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-09 11:26:49 -0700

    sorting alternatives

--- Sorting/sorting.sql
+++ Sorting/sorting.sql
@@ -2,3 +2,12 @@ SELECT *
 FROM products
 -- ORDER BY price;
 ORDER BY price DESC; -- ASC default
+
+SELECT *
+FROM products
+-- ORDER BY name;
+ORDER BY name DESC;
+
+SELECT *
+FROM products
+ORDER BY price, weight;
