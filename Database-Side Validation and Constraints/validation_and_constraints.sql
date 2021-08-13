abbf2dffe253220209e62449ea1a9f3b9263d8b7
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-12 22:40:48 -0700

    multi column validation

--- Database-Side Validation and Constraints/validation_and_constraints.sql
+++ Database-Side Validation and Constraints/validation_and_constraints.sql
@@ -68,3 +68,19 @@ ADD CHECK (price > 0);
 
 INSERT INTO products (name, department, price, weight)
 VALUES ('Belt', 'Clothes', -99, 1); -- error
+
+-- validation multiple columns
+
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	est_delivery TIMESTAMP NOT NULL,
+	CHECK (created_at < est_delivery)
+);
+
+INSERT INTO orders (name, created_at, est_delivery)
+VALUES ('Shirt', '2000-NOV-20 01:00AM', '2000-NOV-25 1:00AM');
+
+INSERT INTO orders (name, created_at, est_delivery)
+VALUES ('Shirt', '2000-NOV-20 01:00AM', '2000-NOV-10 1:00AM'); -- error
