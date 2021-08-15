4e8297010c5f175908df10b2fffa776e155e6425
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-14 22:59:46 -0700

    followers

--- Database Design/database_design.sql
+++ Database Design/database_design.sql
@@ -79,3 +79,11 @@ CREATE TABLE hashtag_posts (
     post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     UNIQUE(hashtag_id, post_id)
 );
+
+CREATE TABLE followers (
+    id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    leader_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    follower_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE(leader_id, follower_id)
+);
