dd745905f8701b43cf6e065be250354fcbcf17b3
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-14 22:53:51 -0700

    hashtags

--- Database Design/database_design.sql
+++ Database Design/database_design.sql
@@ -59,10 +59,16 @@ CREATE TABLE photo_tags (
     UNIQUE(user_id, post_id)
 );
 
-CREATE TABLE photo_tags (
+CREATE TABLE caption_tags (
     id SERIAL PRIMARY KEY,
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     UNIQUE(user_id, post_id)
+);
+
+CREATE TABLE hashtags (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(20) NOT NULL UNIQUE
 );
