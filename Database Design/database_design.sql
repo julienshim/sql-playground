463a5630d9b9f0d9e9094f4a0f84b3ef6d606256
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-14 22:48:58 -0700

    photo_tags

--- Database Design/database_design.sql
+++ Database Design/database_design.sql
@@ -44,5 +44,17 @@ CREATE TABLE likes (
         +
         COALESCE((comment_id)::BOOLEAN::INTEGER, 0)
         = 1
-    )
+    ),
+    UNIQUE(user_id, post_id, comment_id)
 );
+
+CREATE TABLE photo_tags (
+    id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
+    UNIQUE(user_id, post_id)
+)
