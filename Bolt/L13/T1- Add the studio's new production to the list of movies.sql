6b16448103486e5dd07902b0ba1c280e6219d9e2
julienshim <25723428+julienshim@users.noreply.github.com>
2021-08-30 11:48:09 -0700

    L13 T1

---
+++ Bolt/L13/T1- Add the studio's new production to the list of movies.sql
@@ -0,0 +1,6 @@
+-- Add the studio's new production,
+-- Toy Story 4 to the list of movies 
+-- (you can use any director) 
+
+INSERT INTO movies (title, director, year, length_minutes)
+VALUES ('Toy Story 4', 'Josh Cooley', 2019, 100);
