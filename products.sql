a189a5baab6a402d1756405ba20f1e3cba0b45c2
julienshim <25723428+julienshim@users.noreply.github.com>
2023-09-02 21:58:35 -0700

    p2

--- products.sql
+++ products.sql
@@ -12,3 +12,22 @@ ORDER BY price, weight DESC; -- price (ASC), weight DESC
 
 SELECT COUNT(*) -- 50
 FROM users;
+
+SELECT *
+FROM users
+OFFSET 40; -- 41 through 50 (skip the first 40)
+
+SELECT * -- records 4 through 5
+FROM users
+LIMIT 2 -- only give the first 2 rows of the result
+OFFSET 3; -- skip the first 3 rows of the results
+
+SELECT * -- 5 least exp
+FROM products
+ORDER BY price
+LIMIT 5;
+
+SELECT * -- 5 most exp
+FROM products
+ORDER BY price DESC
+LIMIT 5;
