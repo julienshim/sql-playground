043281e5e7ff256cced5e866e41d9a85d38b132b
julienshim <25723428+julienshim@users.noreply.github.com>
2023-09-06 22:01:04 -0700

    p6

--- products.sql
+++ products.sql
@@ -71,3 +71,17 @@ FROM products;
     ORDER BY price / weight DESC
     LIMIT 4
 );
+
+(
+    SELECT *
+    FROM products
+    ORDER BY price DESC
+    LIMIT 4
+)
+EXCEPT -- find items not in latter inquiry
+(
+    SELECT * 
+    FROM products
+    ORDER BY price / weight DESC
+    LIMIT 4
+);
